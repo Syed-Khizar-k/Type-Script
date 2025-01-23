9c162dd83c4c7f77ef63b2e0acc96cab3ea92973
Syed-khizar-k <khizarsyed4294@gmail.com>
2025-01-23 12:59:34 +0500

    export File of functions

---
+++ Advance-TS/calculator.ts
@@ -0,0 +1,19 @@
+export function add(num1: number, num2: number): number {
+  let add: number = num1 + num2;
+  return add;
+}
+
+export function substract(num1: number, num2: number): number {
+  let subs: number = num1 - num2;
+  return subs;
+}
+
+export function multi(num1: number, num2: number): number {
+  let multi: number = num1 * num2;
+  return multi;
+}
+
+export function divi(num1: number, num2: number): number {
+  let divi: number = num1 / num2;
+  return divi;
+}
