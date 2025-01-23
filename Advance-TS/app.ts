80cc8f9923513609b69c80ac000f6973312fb88b
Syed-khizar-k <khizarsyed4294@gmail.com>
2025-01-23 13:00:00 +0500

    import file of functions

---
+++ Advance-TS/app.ts
@@ -0,0 +1,13 @@
+import { add, multi, divi, substract } from "./calculator";
+
+let addition = add(1, 2);
+console.log(addition);
+
+let substraction = substract(6, 4);
+console.log(substraction);
+
+let multiplication = multi(3, 4);
+console.log(multiplication);
+
+let division = divi(6, 3);
+console.log(division);
